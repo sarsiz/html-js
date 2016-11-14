aee413e4a4fcf16342c89b0a9c0b8ea30b650d8a
Sarsiz Chauhan <sarsiz97@gmail.com>
2016-11-14 14:17:22 +0530

    Updated

--- playlist.js
+++ playlist.js
@@ -3,23 +3,27 @@ window.onload = init;
 function init(){
 	var button = document.getElementById("addButton");
 	button.onclick = handleButtonClick;
+	loadPlaylist();
 }
 
 function handleButtonClick(){
 	var textInput = document.getElementById("songTextInput");
 	var songName = textInput.value;
 	
+	//first create the new element li where the song name is goin to go
+	var li = document.createElement("li");
+	li.innerHTML = songName;
+
+	var ul = document.getElementById("playlist");
+	ul.appendChild(li);
+
+	save(songName);
+
 	if(songName == ""){
 		alert("Please enter a song");
 	} else{
 		alert("Adding " + songName);
 	}
 	
-//first create the new element li where the song name is goin to go
-	var li = document.createElement("li");
-	li.innerHTML = songName;
-
-	var ul = document.getElementById("playlist");
-	ul.appendChild(li);
 
 }
